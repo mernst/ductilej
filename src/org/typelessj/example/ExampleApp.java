ea78defadc888f4e155c2ded8b00eb46ddc62ecd
samskivert <devnull@localhost>
2009-09-15 23:27:37 +0000

    Further progress on an approach that will be totally abandoned, but which I want to get to a particular point before leaving it. Need to finish (partially) proper method resolution.

--- src/org/typelessj/example/ExampleApp.java
+++ src/org/typelessj/example/ExampleApp.java
@@ -8,20 +8,24 @@ package org.typelessj.example;
  */
 public class ExampleApp
 {
+    public static class Tester
+    {
+        public int compute (int value) {
+            return value * 2 + 5;
+        }
+    }
+
     public static void main (String[] args)
     {
+        Tester tester = new Tester();
+
         int age = 25;
-        int value = compute(age);
+        Object value = tester.compute(age);
         System.out.println("Value " + value);
 
         String name = "Phineas P. Gage";
         String text = append(name);
         System.out.println("Text " + text);
-    }
-
-    protected static int compute (int value)
-    {
-        return value * 2 + 5;
     }
 
     protected static String append (String value)
