484d7e9acac3e70df5759510799098656e3a30d6
samskivert <devnull@localhost>
2009-11-06 22:21:16 +0000

    Tests for various types of assignment and hashcode overriding.

---
+++ src/org/typelessj/tests/AssignTest.java
@@ -0,0 +1,35 @@
+//
+// $Id$
+
+package org.typelessj.tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests various assignment transformations.
+ */
+public class AssignTest
+{
+    public class MutableInt {
+        public int value;
+        public MutableInt (int value) {
+            this.value = value;
+        }
+    }
+
+    @Test
+    public void assignTest ()
+    {
+        int ivalue = 42;
+        assertTrue(ivalue == 42);
+        String svalue = "hello";
+        assertEquals(svalue, "hello");
+        int[] iarray = new int[1];
+        iarray[0] = 42;
+        assertEquals(iarray[0], 42);
+        MutableInt iobj = new MutableInt(42);
+        iobj.value = -24;
+        assertEquals(iobj.value, -24);
+    }
+}
