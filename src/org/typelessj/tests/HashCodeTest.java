ee10ec5f243af34a4c13997d429f1c2d1ac3c7e5
samskivert <samskivert@localhost>
2009-11-06 22:21:16 +0000

    Tests for various types of assignment and hashcode overriding.

--- src/org/typelessj/tests/HashCodeTest.java
+++ src/org/typelessj/tests/HashCodeTest.java
@@ -3,9 +3,45 @@
 
 package org.typelessj.tests;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
- * Does something extraordinary.
+ * Tests (non-)transformation of hashcode signature.
  */
 public class HashCodeTest
 {
+    public class Key
+    {
+        public /* final TODO: this hoses the constructor */ int one, two;
+
+        public Key (int one, int two) {
+            this.one = one;
+            this.two = two;
+        }
+
+        @Override
+        public boolean equals (Object other) {
+            return (other instanceof Key) &&
+                ((Key)other).one == this.one &&
+                ((Key)other).two == this.two;
+        }
+
+        @Override
+        public int hashCode () {
+            return 37 * this.one + this.two;
+        }
+
+        @Override
+        public String toString () {
+            return "[" + this.one + ", " + this.two + "]";
+        }
+    }
+
+    @Test
+    public void hashCodeTest ()
+    {
+        Key key = new Key(42, -24);
+        assertTrue(key.hashCode() == (37 * 42 + -24));
+    }
 }
