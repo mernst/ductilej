803125be5b56ce36d07e44d3a9e7ee253e7c6ae0
samskivert <devnull@localhost>
2009-11-10 01:16:02 +0000

    Add a test that highlights our current problems with inner classes.

--- src/org/typelessj/tests/InnerClassTest.java
+++ src/org/typelessj/tests/InnerClassTest.java
@@ -3,6 +3,9 @@
 
 package org.typelessj.tests;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,7 +19,7 @@ public class InnerClassTest
         int op (int value);
     }
 
-    @Test public void testAnonInterface () {
+    @Test public void testDetypedInterface () {
         Value v = new Value() {
             public int value () {
                 return 5;
@@ -26,5 +29,16 @@ public class InnerClassTest
             }
         };
         assertEquals(v.op(v.value()), 25);
+    }
+
+    @Test public void testNonDetypedInterface () {
+        Integer[] values = new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        Arrays.sort(values, new Comparator<Integer>() {
+            public int compare (Integer a, Integer b) {
+                return (a == b) ? 0 : ((a < b) ? -1 : 1);
+            }
+        });
+        assertEquals(values[0], (Integer)1);
+        assertEquals(values[2], (Integer)3);
     }
 }
