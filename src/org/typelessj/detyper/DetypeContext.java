ca0ee9cfe060dc1265c1ed9848b8a3c33957945b
samskivert <samskivert@localhost>
2009-11-10 01:52:59 +0000

    Provide a workaround for determining whether methods of anonymous inner classes should be detyped. This whole process is greatly complicated by the fact that javac does not enter anonymous inner classes (indeed, it rewrites all classes to outer classes and then reruns enter on them but that happens long after we need it to). To work around this (for now) we assume that if an anonymous inner class extends a library class, all of its methods are library methods and if it extends a detyped class, all of its methods are detyped. One or both of those will undoubtedly come back to bite us at some point but for now we want to try to make other progress.

--- src/org/typelessj/detyper/DetypeContext.java
+++ src/org/typelessj/detyper/DetypeContext.java
@@ -4,6 +4,7 @@
 package org.typelessj.detyper;
 
 import com.sun.tools.javac.code.Scope;
+import com.sun.tools.javac.code.Symbol;
 
 /**
  * Used to compute scopes during the detyping process.
@@ -13,6 +14,9 @@ public class DetypeContext
     /** The symbols in scope in this context. */ 
     public Scope scope = null;
 
+    /** The parent of the anonymous inner class currently being created, if any. */
+    public Symbol anonParent;
+
     /**
      * Duplicates this context with the specified new scope.
      */
@@ -20,6 +24,7 @@ public class DetypeContext
     {
         DetypeContext ctx = new DetypeContext();
         ctx.scope = scope;
+        ctx.anonParent = anonParent;
         return ctx;
     }
 }
