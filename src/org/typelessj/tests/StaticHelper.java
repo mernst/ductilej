a9f37e5cc2a7d88bb43dce33687b21a4b6dce8ab
samskivert <devnull@localhost>
2009-11-11 02:33:57 +0000

    More pathology to satisfy your pathological urges.

--- src/org/typelessj/tests/StaticHelper.java
+++ src/org/typelessj/tests/StaticHelper.java
@@ -10,6 +10,12 @@ public class StaticHelper
 {
     public static final int HELPFUL = 42;
 
+    public static class Inner {
+        public static int innerHelp (int value) {
+            return value + 24;
+        }
+    }
+
     public static int help (int value)
     {
         return value + 42;
