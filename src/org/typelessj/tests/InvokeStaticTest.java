acbb6ce5d66e0b2ba67d073922034f3aa5db81d9
samskivert <samskivert@localhost>
2009-11-12 23:57:00 +0000

    assertEquals takes (expected, got) so put things in that order.

--- src/org/typelessj/tests/InvokeStaticTest.java
+++ src/org/typelessj/tests/InvokeStaticTest.java
@@ -19,35 +19,35 @@ public class InvokeStaticTest
 
         // a static method defined on an inner class
         int value = Tester.half(25);
-        assertEquals(value, 12);
+        assertEquals(12, value);
 
         // a static method defined on an implicit Java class
         Integer foo = Integer.valueOf(25);
-        assertEquals(foo, (Integer)25); // this cast needed for normal compilation
+        assertEquals((Integer)25, foo); // this cast needed for normal compilation
 
         // a static method defined on this class
         value = triple(42);
-        assertEquals(value, 126);
+        assertEquals(126, value);
 
         // a static method of an inner class calling one in neighboring scope
         value = MoreTester.threeHalves(42);
-        assertEquals(value, 63);
+        assertEquals(63, value);
 
         // a static method of an inner class calling one from broader scope
         value = InvokeStaticTest.Tester.tripleDouble(42);
-        assertEquals(value, 252);
+        assertEquals(252, value);
 
         // test finding a qualified inner inner class
         value = MoreTester.InnerTester.oneFourth(42);
-        assertEquals(value, 10);
+        assertEquals(10, value);
 
         // test a static method of another class in this package
         value = StaticHelper.help(1);
-        assertEquals(value, 43);
+        assertEquals(43, value);
 
         // test a static method of another class in this package
         value = StaticHelper.Inner.innerHelp(1);
-        assertEquals(value, 25);
+        assertEquals(25, value);
 
         // TODO: test statically imported method (or just use assertTrue());
     }
@@ -76,7 +76,7 @@ public class InvokeStaticTest
 
         protected static class InnerTester {
             public static int oneFourth (int value) {
-                return value;
+                return value/4;
             }
         }
     }
