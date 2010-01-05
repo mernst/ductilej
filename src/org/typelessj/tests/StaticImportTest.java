4f9dc07166e3e5007bdbfbeabe3700d62e49dd3d
samskivert <samskivert@localhost>
2010-01-05 20:46:37 +0000

    Add test for imported method (even though every test uses statically imported methods from org.junit.Assert).

--- src/org/typelessj/tests/StaticImportTest.java
+++ src/org/typelessj/tests/StaticImportTest.java
@@ -7,14 +7,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import static org.typelessj.tests.StaticHelper.inner;
+import static org.typelessj.tests.StaticHelper.Inner.innerHelp;
 
 /**
  * Tests static imports.
  */
 public class StaticImportTest
 {
-    @Test public void testStaticImportedField ()
+    @Test public void testImportedField ()
     {
         assertTrue(inner.triple(5) == 15);
+    }
+
+    @Test public void testImportedMethod ()
+    {
+        assertTrue(innerHelp(5) == 29);
     }
 }
