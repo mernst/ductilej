627bbaf00c49b6023359eda9f9aa1e8233facf33
samskivert <samskivert@localhost>
2010-01-10 19:26:32 +0000

    When we're detyping a method application of the for expr.method(args) "expr.method" is represented in the AST as a field selection, but we don't want to transform that into a reflective field lookup when we call super.visitApply() so we have a chec kin visitSelect() that prevents the transformation of the Apply.meth node.
    However if expr *itself* is a field selection (foo.bar.method()) then we *do*
    want to transform the lower field selection node(s). Annoyingly this means we
    have to do this manually in visitApply().
    
    Alternatively I could come up with some complicated check to indicate that
    Apply.meth.Select is not to be transformed but
    Apply.meth.Select.Select[.Select...] is to be transformed.

--- src/org/typelessj/tests/SelectTest.java
+++ src/org/typelessj/tests/SelectTest.java
@@ -18,6 +18,7 @@ public class SelectTest
         Value five = new Value(5);
         assertTrue(five.value == 5);
         assertTrue(toValue(19).value == 19);
+        assertTrue(five.someText.length() == 10);
         // SelectTest.Value shouldn't be transformed, expr.value should
         assertTrue(new SelectTest.Value(180).value == 180);
         // since SelectTest.Value is a class name, expr.theAnswer is not xformed
@@ -36,6 +37,8 @@ public class SelectTest
         public static int theAnswer = 42;
 
         public int value;
+
+        public String someText = "Some text!";
 
         public Value (int value) {
             this.value = value;
