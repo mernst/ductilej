6745a65fdd4db04a257944e2f9262910c16e3603
samskivert <samskivert@localhost>
2010-02-07 18:24:57 +0000

    Handle resolution of wildcard types.

--- src/org/typelessj/tests/TypeVarTest.java
+++ src/org/typelessj/tests/TypeVarTest.java
@@ -4,6 +4,8 @@
 package org.typelessj.tests;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -23,6 +25,15 @@ public class TypeVarTest
     @Test public void testSuperCast () {
         MyList<Integer> list = new MyList<Integer>();
         list.add(5);
-        assertEquals(list.get(0), Integer.valueOf(5));
+        assertEquals(Integer.valueOf(5), list.get(0));
+    }
+
+    @Test public void testWildcard () {
+        Class<?> clazz = TypeVarTest.class;
+        assertEquals("TypeVarTest", clazz.getSimpleName());
+
+        Map<Class<?>, Integer> foo = new HashMap<Class<?>, Integer>();
+        foo.put(TypeVarTest.class, 5);
+        assertEquals(Integer.valueOf(5), foo.get(TypeVarTest.class));
     }
 }
