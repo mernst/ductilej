36cdbd886fa08cafc7833603626f429d188385e6
samskivert <samskivert@localhost>
2010-02-12 21:40:03 +0000

    Make sure things work when we've specified a concrete type for our parameter.

--- src/org/typelessj/tests/RetTypeVarTest.java
+++ src/org/typelessj/tests/RetTypeVarTest.java
@@ -11,8 +11,15 @@ import static org.junit.Assert.*;
 public class RetTypeVarTest
 {
     public static class MyList<T> extends ArrayList<T> {
-        public T get (int index) {
+        @Override public T get (int index) {
             T val = super.get(index);
+            return val;
+        }
+    }
+
+    public static class MyIntList extends ArrayList<Integer> {
+        @Override public Integer get (int index) {
+            Integer val = super.get(index);
             return val;
         }
     }
@@ -22,5 +29,9 @@ public class RetTypeVarTest
         MyList<Integer> list = new MyList<Integer>();
         list.add(5);
         assertEquals(Integer.valueOf(5), list.get(0));
+
+        MyIntList ilist = new MyIntList();
+        ilist.add(5);
+        assertEquals(Integer.valueOf(5), ilist.get(0));
     }
 }
