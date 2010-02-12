6c7eff826fd6eed9912798e39fd2e729ea400359
samskivert <samskivert@localhost>
2010-02-12 21:27:58 +0000

    More operator testing.

--- src/org/typelessj/tests/OperatorTest.java
+++ src/org/typelessj/tests/OperatorTest.java
@@ -3,6 +3,8 @@
 
 package org.typelessj.tests;
 
+import java.io.File;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,39 +16,53 @@ public class OperatorTest
     @Test public void testUnary ()
     {
         int ii = 1;
-        assertEquals(ii++, 1);
-        assertEquals(++ii, 3);
-        assertEquals(ii--, 3);
-        assertEquals(--ii, 1);
+        assertEquals(1, ii++);
+        assertEquals(3, ++ii);
+        assertEquals(3, ii--);
+        assertEquals(1, --ii);
 
         int[] values = new int[1];
-        assertEquals(values[0]++, 0);
-        assertEquals(++values[0], 2);
-        assertEquals(values[0]--, 2);
-        assertEquals(--values[0], 0);
+        assertEquals(0, values[0]++);
+        assertEquals(2, ++values[0]);
+        assertEquals(2, values[0]--);
+        assertEquals(0, --values[0]);
 
         Value value = new Value();
-        assertEquals(value.value++, 0);
-        assertEquals(++value.value, 2);
-        assertEquals(value.value--, 2);
-        assertEquals(--value.value, 0);
+        assertEquals(0, value.value++);
+        assertEquals(2, ++value.value);
+        assertEquals(2, value.value--);
+        assertEquals(0, --value.value);
     }
 
     @Test public void testAssignOp ()
     {
         int ii = 1;
         ii += 1;
-        assertEquals(ii, 2);
+        assertEquals(2, ii);
         ii -= 1;
-        assertEquals(ii, 1);
+        assertEquals(1, ii);
 
         int[] values = new int[1];
         values[0] += 1;
-        assertEquals(values[0], 1);
+        assertEquals(1, values[0]);
 
         Value value = new Value();
         value.value += 1;
-        assertEquals(value.value, 1);
+        assertEquals(1, value.value);
+    }
+
+    @Test public void testStringPromote ()
+    {
+        assertEquals("1", ""+1);
+        assertEquals("1", 1+"");
+        // TODO: what to do with nulls at runtime? always assume string concat?
+        // assertEquals("null1", (String)null+1);
+        // assertEquals("1null", 1+(String)null);
+        assertEquals("1", ""+'1');
+        assertEquals("1", '1'+"");
+
+        // make sure type of '~' + File.separator is String
+        assertTrue(("~" + File.separator).startsWith('~' + File.separator));
     }
 
     protected static class Value {
