2069344fa13306ae27afe5e5325ab6e4b8effc5e
samskivert <devnull@localhost>
2010-02-27 23:40:52 +0000

    Since we're not turning enum field initializer expressions into reflective constructor calls, we have to manually insert type carrying arguments just as we would for an unreflectable this() or super() call.

--- src/org/typelessj/tests/EnumTest.java
+++ src/org/typelessj/tests/EnumTest.java
@@ -10,11 +10,35 @@ public class EnumTest
 {
     public enum TestEnum { A, B, C, D };
 
+    // tests restoring static type in Enum constructors
+    public enum StringEnum {
+        A(get("A")),
+        B(get("B")),
+        C(get("C"));
+
+        StringEnum (String string) {
+            _string = string;
+        }
+
+        public String toString () {
+            return _string;
+        }
+
+        protected String _string;
+    }
+
     @Test public void testValueOf ()
     {
         Class<?> base_type = TestEnum.class;
         @SuppressWarnings({"unchecked","rawness","rawtypes"})
         Object eval = Enum.valueOf((Class<? extends Enum>)base_type, "B");
         assertEquals(TestEnum.B, eval);
+
+        assertEquals("Bgot", StringEnum.B.toString());
+    }
+
+    protected static String get (String value)
+    {
+        return value + "got";
     }
 }
