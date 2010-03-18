3b7c0efc1e90023b44aeee26bbec308a52053f82
Michael Bayne <mdb@samskivert.com>
2010-03-17 21:25:59 -0700

    More failing tests. Whee!

--- src/org/ductilej/tests/PromoteTest.java
+++ src/org/ductilej/tests/PromoteTest.java
@@ -11,20 +11,42 @@ import static org.junit.Assert.*;
  */
 public class PromoteTest
 {
-    @Test public void testPromote ()
+    @Test public void testNarrow ()
+    {
+        short value = 1; // int literal narrowed to short
+        assertEquals((short)1, value);
+
+        value = FIVE;
+        assertEquals((short)5, value);
+    }
+
+    @Test public void testShortPromote ()
+    {
+        short val1 = 1, val2 = 2;
+        Object val = val1 + val2; // + result is numeric promoted to int
+        assertEquals(Integer.class, val.getClass());
+    }
+
+    @Test public void testLongNonPromote ()
+    {
+        long val1 = 1, val2 = 2;
+        Object val = val1 + val2; // + result remains long
+        assertEquals(Long.class, val.getClass());
+    }
+
+    @Test public void testTypingAndRuntimeNonPromote ()
     {
         // ensure that during detyping the type of _nextInternCode++ resolves to short not int, and
         // ensures that at runtime we also don't inadvertently promote the short to an int
-        short value = createInternMapping(_nextInternCode++);
+        short value = box(_counter++);
         assertEquals(1, value);
     }
 
-    protected Short createInternMapping (short code)
+    protected Short box (short code)
     {
         return code;
     }
 
-    // we're also testing the implicit narrowing that takes place during this initializer
-    // assignment (1 is an integer literal and it is narrowed to a short on assignment)
-    protected short _nextInternCode = 1;
+    protected short _counter = 1;
+    protected static final int FIVE = 5;
 }
