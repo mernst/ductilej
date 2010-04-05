f920647aaa01fe55b3054b1cb4b0d5fb52b9128c
Michael Bayne <mdb@samskivert.com>
2010-04-04 18:07:34 -0700

    etype -> otype to match naming in visitApply.

--- src/org/ductilej/detyper/Detype.java
+++ src/org/ductilej/detyper/Detype.java
@@ -552,11 +552,11 @@ public class Detype extends PathedTreeTranslator
                 // in which case "this" is C.this but we really need A.this to instantiate a B, so
                 // we have to resolve the "outer" type of the class we're instantiating and compare
                 // it to the current enclosing class
-                Type etype = _types.erasure(ctype.getEnclosingType());
-                if (_types.isSubtype(_types.erasure(_env.enclClass.sym.type), etype)) {
+                Type otype = _types.erasure(ctype.getEnclosingType());
+                if (_types.isSubtype(_types.erasure(_env.enclClass.sym.type), otype)) {
                     thisex = _tmaker.at(tree.pos).Ident(_names._this);
                 } else {
-                    thisex = _tmaker.at(tree.pos).Select(typeToTree(etype, tree.pos), _names._this);
+                    thisex = _tmaker.at(tree.pos).Select(typeToTree(otype, tree.pos), _names._this);
                 }
             }
             args = args.prepend(thisex).prepend(classLiteral(tree.clazz, tree.clazz.pos));
