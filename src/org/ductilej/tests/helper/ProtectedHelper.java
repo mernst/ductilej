3060768fbb1149521e5de044eec581ccede10b15
Michael Bayne <mdb@samskivert.com>
2010-05-04 10:00:07 -0700

    Tests for anonymous protected constructor resolution.

---
+++ src/org/ductilej/tests/helper/ProtectedHelper.java
@@ -0,0 +1,16 @@
+//
+// $Id$
+
+package org.ductilej.tests.helper;
+
+/**
+ * Used to test access to a protected constructor.
+ */
+public class ProtectedHelper
+{
+    public final String value;
+
+    protected ProtectedHelper (String value) {
+        this.value = value;
+    }
+}
