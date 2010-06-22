13d1858ad69c414bc483444e1e590b53ccd965c4
Michael Bayne <mdb@samskivert.com>
2010-06-22 09:26:49 -0700

    Added Ductile-only tests and an "ant dtests" target for running them.

--- src/org/ductilej/tests/IterableTest.java
+++ src/org/ductilej/tests/IterableTest.java
@@ -28,35 +28,4 @@ public class IterableTest
             assertEquals("One", item);
         }
     }
-
-    // TODO: move these into Ductile-only test suite when we have such a thing
-    // @Test public void testDuckTypedIterable () {
-    //     Object foo = new Object() {
-    //         public Iterator<String> iterator () {
-    //             return _list.iterator();
-    //         }
-    //         protected ArrayList<String> _list = new ArrayList<String>(); {
-    //             _list.add("One");
-    //         }
-    //     };
-    //     for (String item : foo) {
-    //         assertEquals("One", item);
-    //     }
-    // }
-
-    // @Test public void testDuckTypedNonIterable () {
-    //     Object foo = new Object() {
-    //         public String iterator () {
-    //             return "Ohai!";
-    //         }
-    //     };
-    //     try {
-    //         for (String item : foo) {
-    //             assertEquals("One", item);
-    //         }
-    //         fail();
-    //     } catch (IllegalArgumentException iae) {
-    //         // expected, because iterator() returns non-Iterator
-    //     }
-    // }
 }
