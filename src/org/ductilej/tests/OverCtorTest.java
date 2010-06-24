a586527f212df832e992c43028387c5ac29c7d51
Michael Bayne <mdb@samskivert.com>
2010-06-24 10:34:03 -0700

    Another wrinkle with method overload resolution. Such are the dangers of having a type system that is not rooted in a single LUB.

--- src/org/ductilej/tests/OverCtorTest.java
+++ src/org/ductilej/tests/OverCtorTest.java
@@ -30,4 +30,34 @@ public class OverCtorTest
         Foo foo = new Foo((Integer)null);
         assertEquals("null", foo.value);
     }
+
+    public static class A {
+        public final String data;
+
+        public A (long arg) { // detypes to A(Object,long)
+            this.data = String.valueOf(arg);
+        }
+        public A (Object arg) { // detypes to A(Object,Object)
+            this.data = String.valueOf(arg);
+        }
+    }
+
+    public static class B extends A {
+        public B () {
+            super(0L); // detypes to super(0L,0L)
+
+            // prior to detyping, A(long) matches during the subtyping phase and A(Object) matches
+            // only during the method invocation conversion phase (which is never entered because a
+            // match existed during the subtyping phase)
+
+            // after detyping A(Object,long) and A(Object,Object) both match during the method
+            // invocation conversion phase, but there's no subtype relationship between long and
+            // Object, so neither is more specific and they are thus ambiguous
+        }
+    }
+
+    @Test public void testAmbiguousSuperCons () {
+        B b = new B();
+        assertEquals("0", b.data);
+    }
 }
